093ad7aac06e842219ee269c05d8c0e7c2cf49bf
LovesAsuna <qq625924077@gmail.com>
2020-08-22 14:09:37 +0800

    [Refactor] 待完成

---
+++ src/main/java/me/lovesasuna/lanzou/network/Uploadable.java
@@ -0,0 +1,13 @@
+package me.lovesasuna.lanzou.network;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ * @author LovesAsuna
+ * @date 2020/8/22 14:07
+ **/
+public interface Uploadable {
+    //todo 上传操作待完成
+    boolean upload(URL url, File file);
+}
